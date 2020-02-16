08a63fed30f40c7ae62a9ac22c551c7607f0a9d8
Iurii Mednikov <yuri.mednikov@mednikov.net>
2020-02-16 12:27:13 +0100

    Modified Arrays tests

--- src/test/java/net/mednikov/datastructures/arrays/ArraysJavaTest.java
+++ src/test/java/net/mednikov/datastructures/arrays/ArraysJavaTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.Test;
 
@@ -25,6 +26,7 @@ public class ArraysJavaTest{
 
         // step 3. add new element
         names2[oldLength] = name;
+
         assertEquals(name, names2[oldLength]);
     }
 
@@ -32,6 +34,7 @@ public class ArraysJavaTest{
     public void arrayIsObjectTest(){
       int[] numbers = new int[10];
       boolean result = numbers instanceof Object;
+
       assertTrue(result);
     }
 
@@ -39,17 +42,23 @@ public class ArraysJavaTest{
     public void replaceArrayElementTest(){
       int[] numbers = {1,2,3,4,5};
       int i3 = numbers[3];
+
       assertEquals(4, i3);
+
       numbers[3] = 15;
       i3 = numbers[3];
+
       assertEquals(15, i3);
     }
 
     @Test 
     public void filterArrayTest(){
       String[] names = {"Alejandra", "Beatriz", "Aneta", "Carmen", "Ana", "Gabriela", "Alisa"};
+
       assertEquals(7, names.length);
+
       long res = Arrays.stream(names).filter(name->name.startsWith("A")).count();
+
       assertEquals(4, res);
     }
 
@@ -58,6 +67,22 @@ public class ArraysJavaTest{
       int[] numbers = {5, 12, 6, 1, 32};
       Arrays.sort(numbers);
       int i3 = numbers[3];
+
       assertEquals(12, i3);
+
+      Integer[] numbers2 = {15, 98, 1, 9, 14, 32};
+	    Arrays.sort(numbers2, Collections.reverseOrder());
+      int i4 = numbers2[4];
+      
+	    assertEquals(9, i4);
+    }
+
+    @Test
+    public void equalsTest(){
+      int[] numbers = {15, 98, 1, 9, 14, 32};
+      int[] numbers2 = {15, 98, 1, 9, 14, 32};
+      boolean result = Arrays.equals(numbers, numbers2);
+      
+      assertTrue(result);
     }
 }
--- src/test/java/net/mednikov/datastructures/arrays/ArraysVavrTest.java
+++ src/test/java/net/mednikov/datastructures/arrays/ArraysVavrTest.java
@@ -2,6 +2,9 @@ package net.mednikov.datastructures.arrays;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.function.Predicate;
 
 import org.junit.Test;
 
@@ -13,13 +16,18 @@ public class ArraysVavrTest {
     public void addElementsToArrayTest() {
         Array<String> names = Array.of("Alejandra", "Beatriz", "Carmen", "Dolores");
         Array<String> updatedNames = names.append("Isabel");
-        assertFalse(names.equals(updatedNames));
+
+        Predicate<String> predicate = n -> n.equalsIgnoreCase("Isabel");
+
+        assertFalse(names.existsUnique(predicate));
+        assertTrue(updatedNames.existsUnique(predicate));
     }
 
     @Test
     public void accessElementInArrayTest(){
         Array<String> names = Array.of("Alejandra", "Beatriz", "Carmen", "Dolores");
         String name = names.get(2);
+
         assertEquals("Carmen", name);
     }
 
@@ -27,13 +35,22 @@ public class ArraysVavrTest {
     public void filterArrayTest() {
         Array<String> names = Array.of("Alejandra", "Beatriz", "Aneta", "Carmen", "Ana", "Gabriela", "Alisa");
         Array<String> filtered = names.filter(name -> name.startsWith("A"));
+
         assertEquals(4, filtered.length());
+    }
+
+    @Test
+    public void getLengthTest(){
+        Array<String> names = Array.of("Alejandra", "Beatriz", "Carmen", "Dolores");
+
+        assertEquals(4, names.length());
     }
 
     @Test
     public void sortTest(){
         Array<Integer> numbers = Array.ofAll(16, 22, 4, 5, 19, 7, 80);
         Array<Integer> sorted = numbers.sorted();
+
         assertEquals(4, sorted.get(0).intValue());
         assertEquals(5, sorted.get(1).intValue());
         assertEquals(7, sorted.get(2).intValue());
@@ -41,5 +58,28 @@ public class ArraysVavrTest {
         assertEquals(19, sorted.get(4).intValue());
         assertEquals(22, sorted.get(5).intValue());
         assertEquals(80, sorted.get(6).intValue());
+    }
+
+    @Test
+    public void equalsTest(){
+        Array<Integer> array1 = Array.ofAll(16, 22, 4, 5, 19, 7, 80);
+        Array<Integer> array2 = Array.ofAll(16, 22, 4, 5, 19, 7, 80);
+
+        assertTrue(array1.equals(array2));
+    }
+
+    @Test
+    public void replaceTest(){
+        Array<String> names = Array.of("Alejandra", "Beatriz", "Alejandra", "Carmen", "Ana", "Gabriela", "Alejandra");
+
+        Array<String> results1 = names.replace("Alejandra", "Olga");
+        int result1 = results1.count(n -> n.equalsIgnoreCase("Olga"));
+
+        assertEquals(1, result1);
+
+        Array<String> results2 = names.replaceAll("Alejandra", "Maria");
+        int result2 = results2.count(n -> n.equalsIgnoreCase("Maria"));
+
+        assertEquals(3, result2);
     }
 }
