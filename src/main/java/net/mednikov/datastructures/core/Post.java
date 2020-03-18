bd4c2c85355409b7ef6e8b3c0b68dd37b657c71d
Iurii Mednikov <yuri.mednikov@mednikov.net>
2020-03-19 00:30:26 +0100

    Added code for Collections

---
+++ src/main/java/net/mednikov/datastructures/core/Post.java
@@ -0,0 +1,44 @@
+package net.mednikov.datastructures.core;
+
+public class Post {
+
+    private int id;
+    private String title;
+    private String content;
+
+    public Post(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + this.id + "\n Title: " + this.title + "\n Content: " + this.content;
+    }
+
+}
---
+++ src/test/java/net/mednikov/datastructures/collections/CollectionsTest.java
@@ -0,0 +1,99 @@
+package net.mednikov.datastructures.collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+import net.mednikov.datastructures.core.Post;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class CollectionsTest {
+
+    @Test
+    public void insertTest(){
+        // get mock posts
+        List<Post> posts = getPosts();
+
+        // Add single element
+        Post post = new Post(6, "Phasellus scelerisque", "Phasellus scelerisque eros id lacus auctor");
+        posts.add(post);
+        assertThat(posts).contains(post).hasSize(6);
+
+        // add multiple elements
+        List<Post> newPosts = new ArrayList<>();
+        newPosts.addAll(posts);
+        assertThat(newPosts).containsAll(posts);
+    }
+
+    @Test
+    public void removeTest(){
+        // get mock posts
+        List<Post> posts = getPosts();
+
+        Post post = posts.get(2);
+        assertThat(posts).contains(post);
+
+        // remove object
+        posts.remove(post);
+        assertThat(posts).doesNotContain(post);
+
+        // clear
+        posts.clear();
+        assertThat(posts).isEmpty();
+
+        // delete with predicate
+        posts = getPosts();
+        // remove posts with ID 2 and 4
+        posts.removeIf(p -> p.getId() % 2 == 0);
+        assertThat(posts).hasSize(3);
+    }
+
+    @Test
+    public void streamTest(){
+        List<Post> posts = getPosts();
+        Stream<Post> stream = posts.stream();
+        assertThat(stream).isInstanceOf(Stream.class);
+    }
+
+    @Test
+    public void iterationTest(){
+        List<Post> posts = getPosts();
+
+        // create iterator
+        Iterator<Post> iterator = posts.iterator();
+        // Option 1 with while
+        while(iterator.hasNext()){
+            Post post = iterator.next();
+            System.out.println(post);
+        }
+
+        Iterator<Post> iterator2 = posts.iterator();
+        // Option 2 using forEachRemaining
+        iterator2.forEachRemaining(p -> System.out.println(p));
+
+        // using forEach
+        posts.forEach(System.out::println);
+    }
+
+    private List<Post> getPosts(){
+
+        List<Post> posts = new ArrayList<>();
+
+        posts.add(new Post(1, "Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet, consectetur adipiscing elit."));
+
+        posts.add(new Post(2, "Nulla facilisi", "Nulla facilisi. Phasellus suscipit quis ex sit amet pellentesque. Suspendisse potenti."));
+
+        posts.add(new Post(3, "Vivamus eu convallis lorem.", "Vivamus eu convallis lorem. Duis et vulputate leo."));
+
+        posts.add(new Post(4, "Nullam facilisis", "Nullam facilisis elit ut nunc euismod porttitor ac eget neque."));
+
+        posts.add(new Post(5, "Cras nec malesuada", "Cras nec malesuada tellus. Aenean sit amet est id neque"));
+
+        return posts;
+    }
+}
